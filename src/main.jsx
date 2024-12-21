b8a25cb971f9659af36a9e5bc1150daedb75fd98
Deepak Barkhane <deepakbarkhane1610@gmail.com>
2024-12-21 14:10:33 -0800

    project initialized

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootEle = document.querySelector("#root");
+const root = ReactDOM.createRoot(rootEle);
+
+root.render(<App />);
